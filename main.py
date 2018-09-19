a2df67af1556145184049949a5e9a422e92a8a91
Zelun Kong <zelun.kong@outlook.com>
2018-09-19 15:03:37 -0500

    v0.0

---
+++ main.py
@@ -0,0 +1,117 @@
+import os
+import logging
+# import numpy as np
+import torchvision
+import torchvision.transforms as transforms
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import models
+
+
+def setup_logger(level=logging.DEBUG, filename=None):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    if filename is not None:
+        file_handler = logging.FileHandler(filename=filename, mode='a')
+        file_handler.setLevel(logging.INFO)
+        logger.addHandler(file_handler)
+
+    return logger
+
+
+def main():
+    logger = setup_logger(filename='log.txt')
+
+    train_epoch_number = 10
+    batch_size = 100
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # dataset = np.loadtxt(models.Config.dataset_path, delimiter=',')
+    # np.random.shuffle(dataset[:5000])
+
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    # trainset = models.DataSet(dataset[:5000])
+    trainset = torchvision.datasets.CIFAR10(root='./data/cifar10', train=True, download=False, transform=transform)
+    trainloader = DataLoader(dataset=trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    # testset = models.DataSet(dataset[5000:])
+    testset = torchvision.datasets.CIFAR10(root='./data/cifar10', train=False, download=False, transform=transform)
+    testloader = DataLoader(dataset=testset, batch_size=1, shuffle=False, num_workers=2)
+
+    prototypes = {}
+
+    # net = models.CNNNet(device=device)
+    net = models.DenseNet(device=device, number_layers=8, growth_rate=12, drop_rate=0.0)
+    logger.info("DenseNet Channels: %d", net.channels)
+
+    gcpl = models.GCPLLoss(threshold=models.Config.threshold, gamma=models.Config.gamma, lambda_=0.001)
+    sgd = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    if not os.path.exists("pkl"):
+        os.mkdir("pkl")
+
+    if os.path.exists(models.Config.pkl_path):
+        state_dict = torch.load(models.Config.pkl_path)
+        try:
+            net.load_state_dict(state_dict)
+            logger.info("Load state from file %s.", models.Config.pkl_path)
+        except RuntimeError:
+            logger.error("Loading state from file %s failed.", models.Config.pkl_path)
+
+    for epoch in range(train_epoch_number):
+        logger.info("Trainset size: %d, Epoch number: %d", len(trainset), epoch + 1)
+
+        running_loss = 0.0
+
+        for i, (features, labels) in enumerate(trainloader):
+            features = features.to(net.device)
+            sgd.zero_grad()
+            features = net(features).view(batch_size, 1, -1)
+            loss = gcpl(features, labels, prototypes)
+            loss.backward()
+            sgd.step()
+
+            running_loss += loss.item() / batch_size
+
+            logger.debug("[%3d, %5d] loss: %7.4f", epoch + 1, i + 1, loss.item() / batch_size)
+
+        torch.save(net.state_dict(), models.Config.pkl_path)
+
+        prototype_count = 0
+
+        for c in prototypes:
+            prototype_count += len(prototypes[c])
+
+        logger.info("Prototypes Count: %d", prototype_count)
+
+        # if (epoch + 1) % 5 == 0:
+        distance_sum = 0.0
+        correct = 0
+
+        for i, (feature, label) in enumerate(testloader):
+            feature = net(feature.to(net.device)).view(batch_size, 1, -1)
+            predicted_label, probability, min_distance = models.predict(feature, prototypes)
+
+            if label == predicted_label:
+                correct += 1
+
+            distance_sum += min_distance
+
+            logger.debug("%5d: Label: %d, Prediction: %d, Probability: %7.4f, Distance: %7.4f, Accuracy: %7.4f",
+                         i + 1, label, predicted_label, probability, min_distance, correct / (i + 1))
+
+        logger.info("Distance Average: %7.4f", distance_sum / len(testloader))
+        logger.info("Accuracy: %7.4f\n", correct / len(testloader))
+
+
+if __name__ == '__main__':
+    main()
---
+++ models.py
@@ -0,0 +1,231 @@
+import torch
+import torch.nn as nn
+from torch import tensor
+from torch.utils.data import Dataset
+import dense_net
+
+
+class Config:
+    class_number = 10
+
+    # dataset_path = 'data/fashion-mnist_train.csv'
+    # pkl_path = "pkl/fashion-mnist.pkl"
+    # tensor_view = (-1, 28, 28)
+    # in_channels = 1
+    dataset_path = 'data/cifar10_train.csv'
+    pkl_path = "pkl/cifar10.pkl"
+    tensor_view = (-1, 32, 32)
+    in_channels = 3
+
+    threshold = 10.0
+
+    # gamma * threshold < 10
+    gamma = 1.0
+
+
+class DataSet(Dataset):
+    def __init__(self, dataset):
+        self.data = []
+
+        for s in dataset:
+            x = (tensor(s[:-1], dtype=torch.float) / 255).view(*Config.tensor_view)
+            y = tensor(s[-1], dtype=torch.long)
+            self.data.append((x, y))
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return len(self.data)
+
+
+class CNNNet(nn.Module):
+    def __init__(self, device):
+        super(CNNNet, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=Config.in_channels, out_channels=10, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.device = device
+        self.to(device)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        return out
+
+
+class DenseNet(nn.Module):
+    def __init__(self, device, number_layers, growth_rate, reduction=2, bottleneck=True, drop_rate=0.0):
+        super(DenseNet, self).__init__()
+
+        channels = 2 * growth_rate
+
+        if bottleneck:
+            block = dense_net.BottleneckBlock
+        else:
+            block = dense_net.BasicBlock
+
+        # 1st conv before any dense block
+        self.conv1 = nn.Conv2d(in_channels=Config.in_channels, out_channels=channels, kernel_size=3, stride=1, padding=1, bias=False)
+
+        # 1st block
+        self.block1 = dense_net.DenseBlock(number_layers, channels, block, growth_rate, drop_rate)
+        channels = channels + number_layers * growth_rate
+        self.trans1 = dense_net.TransitionBlock(channels, channels // reduction, drop_rate)
+        channels = channels // reduction
+
+        # 2nd block
+        self.block2 = dense_net.DenseBlock(number_layers, channels, block, growth_rate, drop_rate)
+        channels = channels + number_layers * growth_rate
+        self.trans2 = dense_net.TransitionBlock(channels, channels // reduction, drop_rate)
+        channels = channels // reduction
+
+        # 3rd block
+        self.block3 = dense_net.DenseBlock(number_layers, channels, block, growth_rate, drop_rate)
+        channels = channels + number_layers * growth_rate
+
+        # global average pooling and classifier
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.pooling = nn.AvgPool2d(kernel_size=2)
+
+        self.channels = channels
+
+        self.device = device
+        self.to(device)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.trans1(self.block1(out))
+        out = self.trans2(self.block2(out))
+        out = self.block3(out)
+        out = self.relu(self.bn1(out))
+        out = self.pooling(out)
+        return out
+
+
+class Prototypes(object):
+    def __init__(self, label):
+        self.label = label
+        self.sample_count = []
+        self.features = []
+
+    def append(self, feature):
+        self.features.append(feature)
+        self.sample_count.append(1)
+
+    def update(self, index, feature):
+        self[index] = (self.features[index] * self.sample_count[index] + feature) / (self.sample_count[index] + 1)
+        self.sample_count[index] += 1
+
+    def __getitem__(self, item):
+        return self.features[item]
+
+    def __setitem__(self, key, value):
+        self.features[key] = value
+
+    def __len__(self):
+        return len(self.features)
+
+
+class GCPLLoss(nn.Module):
+    def __init__(self, threshold, gamma=0.1, lambda_=0.1):
+        super(GCPLLoss, self).__init__()
+
+        self.threshold = threshold
+        self.lambda_ = lambda_
+        self.gamma = gamma
+
+    def forward(self, features, labels, all_prototypes):
+        closest_prototypes = self.assign_prototype(features, labels, all_prototypes)
+
+        dce_loss = 0.0
+        p_loss = 0.0
+        for feature, label, prototype in zip(features, labels, closest_prototypes):
+            probability = compute_probability(feature, label.item(), all_prototypes, gamma=self.gamma)
+            dce_loss += -probability.log()
+            p_loss += compute_distance(prototype, prototype).pow(2)
+
+        return dce_loss + self.lambda_ * p_loss
+
+    def assign_prototype(self, features, labels, all_prototypes):
+        features = tensor(features).detach()
+        closest_prototypes = []
+
+        for feature, label in zip(features, labels):
+            closest_prototype = feature
+            label = label.item()
+            if label not in all_prototypes:
+                all_prototypes[label] = Prototypes(label)
+                all_prototypes[label].append(feature)
+            else:
+                # find closest prototype from prototypes in corresponding class
+                prototypes = torch.cat(all_prototypes[label].features)
+                distances = compute_multi_distance(feature, prototypes)
+                min_distance, closest_prototype_index = distances.min(dim=0)
+
+                if min_distance < self.threshold:
+                    all_prototypes[label].update(closest_prototype_index, feature)
+                    closest_prototype = all_prototypes[label][closest_prototype_index]
+                else:
+                    all_prototypes[label].append(feature)
+
+            closest_prototypes.append(closest_prototype)
+
+        return closest_prototypes
+
+
+compute_distance = nn.PairwiseDistance(p=2, eps=1e-6)
+compute_multi_distance = nn.PairwiseDistance(p=2, eps=1e-6, keepdim=True)
+
+
+def compute_probability(feature, label, all_prototypes, gamma=Config.gamma):
+    one = 0.0
+    probability = 1e-6
+
+    for l in all_prototypes:
+        prototypes = torch.cat(all_prototypes[l].features)
+        distances = compute_multi_distance(feature, prototypes)
+        one += (-gamma * distances.pow(2)).exp().sum()
+
+    prototypes = torch.cat(all_prototypes[label].features)
+    distances = compute_multi_distance(feature, prototypes)
+
+    if one > 0.0:
+        probability += (-gamma * distances.pow(2)).exp().sum() / one
+    else:
+        probability += one
+
+    return probability
+
+
+def find_closest_prototype(feature, all_prototypes):
+    # find closest prototype from all prototypes
+    min_distance = None
+    label = None
+
+    for l in all_prototypes:
+        prototypes = torch.cat(all_prototypes[l].features)
+        distances = compute_multi_distance(feature, prototypes)
+        d = float(distances.min())
+        if min_distance is None or d < min_distance:
+            min_distance = d
+            label = l
+
+    return label, min_distance
+
+
+def predict(feature, all_prototypes):
+    predicted_label, min_distance = find_closest_prototype(feature, all_prototypes)
+    probability = compute_probability(feature, predicted_label, all_prototypes)
+
+    return predicted_label, float(probability), min_distance
